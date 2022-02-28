62a94b9a1e1bf1febbaf1af7668e7d257c7cb424
xiaoqiu <2904862402@qq.com>
2022-02-28 14:10:15 +0800

    pcl::StatisticalOutlierRemoval

---
+++ pcl_stu/voxel_grid/build/CMakeFiles/voxel_grid.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voxel_grid.dir/voxel_grid.cpp.o"
+  "voxel_grid.pdb"
+  "voxel_grid"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/voxel_grid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
