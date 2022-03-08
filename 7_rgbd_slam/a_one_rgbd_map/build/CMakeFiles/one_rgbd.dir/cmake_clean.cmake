a82c7315dd7043b3dda56531bbdee4feade24b1f
xiaoqiu <2904862402@qq.com>
2022-03-08 19:32:06 +0800

    a_one_rgbd

---
+++ 7_rgbd_slam/a_one_rgbd_map/build/CMakeFiles/one_rgbd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/one_rgbd.dir/one_rgbd.cpp.o"
+  "one_rgbd.pdb"
+  "one_rgbd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/one_rgbd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
