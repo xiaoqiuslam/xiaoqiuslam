f1b7e5fc41ea56c24ec6c541c2f5fba1c7c36b03
xiaoqiu <2904862402@qq.com>
2022-03-08 19:10:35 +0800

    5_point_cloud

---
+++ 5_point_cloud/build/CMakeFiles/loadPCDFile.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loadPCDFile.dir/loadPCDFile.cpp.o"
+  "loadPCDFile.pdb"
+  "loadPCDFile"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/loadPCDFile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
