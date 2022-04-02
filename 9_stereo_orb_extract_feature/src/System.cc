ccc3cd5bc9465c205ec3902ebb3b6e09e47a2693
xiaoqiu <2904862402@qq.com>
2022-04-02 11:14:36 +0800

    update System.cc

--- 9_stereo_orb_extract_feature/src/System.cc
+++ 9_stereo_orb_extract_feature/src/System.cc
@@ -4,10 +4,13 @@
 namespace ORB_SLAM2 {
     System::System(const string &strSettingsFile){
         cv::FileStorage fsSettings(strSettingsFile.c_str(), cv::FileStorage::READ);
+
+        // 4. 创建 ORB_SLAM2::Tracking 对象
         mpTracker = new Tracking(this, strSettingsFile);
     }
 
     cv::Mat System::TrackStereo(const cv::Mat &imLeft, const cv::Mat &imRight) {
+        // 7. 执行 ORB_SLAM2::Tracking 函数 GrabImageStereo
         cv::Mat Tcw = mpTracker->GrabImageStereo(imLeft, imRight);
         return Tcw;
     }
