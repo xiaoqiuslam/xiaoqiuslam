31e77faeb7560d378314163e0bc06bbd6779278f
xq <xq@example.com>
2022-04-14 10:30:27 +0800

    ORBextractor.cc

--- 9_ORB_SLAM2/Examples/ROS/ORB_SLAM2/build/devel/local_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# generated from catkin/cmake/template/local_setup.sh.in
-
-# since this file is sourced either use the provided _CATKIN_SETUP_DIR
-# or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/q/ORB_SLAM2_XQ/Examples/ROS/ORB_SLAM2/build/devel}
-CATKIN_SETUP_UTIL_ARGS="--extend --local"
-. "$_CATKIN_SETUP_DIR/setup.sh"
-unset CATKIN_SETUP_UTIL_ARGS
