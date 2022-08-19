2af1f71e33a30f3b6f04003a2ed4ecc168fe03ac
xiaoqiuslam <2904862402@qq.com>
2022-08-19 22:41:45 +0800

    小秋SLAM实战教程

--- 3_pcl&ros/b_pcl_ros/catkin_ws_pcl/src/pcl_ros_pkg/src/creat_point_cloud_pub.cpp
+++ 3_pcl&ros/b_pcl_ros/catkin_ws_pcl/src/pcl_ros_pkg/src/creat_point_cloud_pub.cpp
@@ -4,36 +4,48 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 
-int main(int argc, char **argv){
-    ros::init(argc, argv, "point_cloud_node");
+int main(int argc, char **argv)
+{
+    // 初始化名字为 ponit_cloud_node 的节点，可用命令 rosrun rqt_graph rqt_graph 查看到
+    ros::init(argc, argv, "ponit_cloud_node");
     ros::NodeHandle nh;
-    ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>("/point_cloud_pub_topic", 100);
-
+    // 建立ros::Publisher，topic是point_cloud_publisher_topic 可用命令 rosrun rqt_graph rqt_graph 查看到
+    // The second parameter to advertise() is the size of the message queue used for publishing messages.
+    // If messages are published more quickly than we can send them,
+    // the number here specifies how many messages to buffer up before throwing some away.
+    ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>("/point_cloud_topic", 1);
+    // 点云更新频率3Hz 意思是３ms发布一次点云数据
     ros::Rate rate(3);
-    unsigned int num_points = 3;
-
-    pcl::PointCloud<pcl::PointXYZRGB> cloud;
-    cloud.points.resize(num_points);
-    sensor_msgs::PointCloud2 output_msg;
-
-    while (ros::ok())
-    {
-        output_msg.header.stamp = ros::Time::now();
-        for(int i = 0; i < num_points; i++){
-            cloud.points[i].x = i;
-            cloud.points[i].y = i;
-            cloud.points[i].z = i;
-
-            cloud.points[i].r = 0;
-            cloud.points[i].g = 255;
-            cloud.points[i].b = 0;
-
+    // 建立 pcl 点云
+    pcl::PointCloud<pcl::PointXYZRGB> point_cloud;
+    // 点云初始化
+    // 点云数目为 3
+    unsigned int point_size = 3;
+    point_cloud.points.resize(point_size);
+    // 建立ros点云
+    sensor_msgs::PointCloud2 ros_point_cloud;
+    // ros::ok() 检查上面创建的节点是否还在
+    while (ros::ok()){
+        // 调用ros获取时间的接口，获取系统时间
+        ros_point_cloud.header.stamp = ros::Time::now();
+        // 创建三个绿色的点
+        for (int i = 0; i < point_size; i++){
+            point_cloud.points[i].x = i;
+            point_cloud.points[i].y = i;
+            point_cloud.points[i].z = 0;
+            point_cloud.points[i].r = 0; 
+            point_cloud.points[i].g = 255;
+            point_cloud.points[i].b = 0;
         }
-        pcl::toROSMsg(cloud, output_msg);
-        output_msg.header.frame_id = "point_cloud_frame_id";
-        pub.publish(output_msg);
+        // 将pcl点云转化为ros消息发布
+        pcl::toROSMsg(point_cloud, ros_point_cloud);
+        // 发布的点云坐标系
+        ros_point_cloud.header.frame_id = "point_cloud_link";
+        // 发布
+        pub.publish(ros_point_cloud);
         rate.sleep();
     }
     ros::spin();
+
     return 0;
 }
