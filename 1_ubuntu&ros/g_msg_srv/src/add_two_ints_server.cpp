cc6ba46e39c6b9bef71d750e849ae61bd4a76d3e
xiaoqiuslam <2904862402@qq.com>
2022-08-20 15:18:46 +0800

    小秋SLAM实战教程

---
+++ 1_ubuntu&ros/g_msg_srv/src/add_two_ints_server.cpp
@@ -0,0 +1,23 @@
+#include "ros/ros.h"
+#include "msg_srv_pkg/AddTwoInts.h"
+
+bool add(msg_srv_pkg::AddTwoInts::Request  &req, msg_srv_pkg::AddTwoInts::Response &res)
+{
+  res.sum = req.a + req.b;
+  ROS_INFO("request: x=%ld, y=%ld", (long int)req.a, (long int)req.b);
+  ROS_INFO("sending back response: [%ld]", (long int)res.sum);
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  // rosrun g_msg_srv add_two_ints_server
+  ros::init(argc, argv, "add_two_ints_server");
+  ros::NodeHandle n;
+
+  ros::ServiceServer service = n.advertiseService("add_two_ints", add);
+  ROS_INFO("Ready to add two ints.");
+  ros::spin();
+
+  return 0;
+}
