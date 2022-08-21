9ae17dd08425bf1f480299aecd6b2c4bf0e93b37
xiaoqiuslam <2904862402@qq.com>
2022-08-21 14:13:38 +0800

    addWeight

---
+++ 2_opencv&ros/f_resize/52_pyrUp.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+
+
+
+//-----------------------------------【main( )函数】------------------------------------------
+//		描述：控制台应用程序的入口函数，我们的程序从这里开始
+//-----------------------------------------------------------------------------------------------
+int main( )
+{
+	//载入原始图   
+	Mat srcImage = imread("../51_resize.jpg");  //工程目录下应该有一张名为1.jpg的素材图
+	Mat tmpImage,dstImage;//临时变量和目标图的定义
+	tmpImage=srcImage;//将原始图赋给临时变量
+
+	//显示原始图  
+	imshow("【原始图】", srcImage);  
+	//进行向上取样操作
+	pyrUp( tmpImage, dstImage, Size( tmpImage.cols*2, tmpImage.rows*2 ) );
+	//显示效果图  
+	imshow("【效果图】", dstImage);  
+
+	waitKey(0);  
+
+	return 0;  
+}
