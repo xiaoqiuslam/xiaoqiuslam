fa72b7318762d3e95cdf1e69dfdb37b2f56715c5
xiaoqiuslam <xiaoqiuslam@qq.com>
2022-12-18 16:41:04 +0800

    build and run 6_tf/src/b_tf_pkg/src/static_tf_broadcaster.cpp

--- 6_tf/src/b_tf_pkg/src/static_tf_broadcaster.cpp
+++ 6_tf/src/b_tf_pkg/src/static_tf_broadcaster.cpp
@@ -7,30 +7,29 @@
 int main(int argc, char **argv)
 {
   ros::init(argc,argv, "my_static_tf2_broadcaster");
-  if(argc != 8){
-    ROS_ERROR("usage: static_turtle_tf2_broadcaster child_frame_name x y z roll pitch yaw");
+
+  if(argc != 9){
+    ROS_ERROR("usage: static_tf_broadcaster x y z roll pitch yaw map base_link");
+    // static_tf_broadcaster 0.011 0.048 0.015 0 0 0 map base_link
+    // rosrun tf static_transform_publisher 0.011 0.048 0.015 0 0 0 map base_link 100
     return -1;
   }
 
-  static tf2_ros::StaticTransformBroadcaster static_broadcaster;
-
-  geometry_msgs::TransformStamped static_transformStamped;
-
-  static_transformStamped.header.stamp = ros::Time::now();
-  static_transformStamped.header.frame_id = "world";
-  static_transformStamped.child_frame_id = argv[1];
-  static_transformStamped.transform.translation.x = atof(argv[2]);
-  static_transformStamped.transform.translation.y = atof(argv[3]);
-  static_transformStamped.transform.translation.z = atof(argv[4]);
-
+  tf2_ros::StaticTransformBroadcaster static_broadcaster;
+  geometry_msgs::TransformStamped geometry_msgs_transform_stamped;
+  geometry_msgs_transform_stamped.header.stamp = ros::Time::now();
+  geometry_msgs_transform_stamped.header.frame_id = argv[7];
+  geometry_msgs_transform_stamped.child_frame_id = argv[8];
+  geometry_msgs_transform_stamped.transform.translation.x = atof(argv[1]);
+  geometry_msgs_transform_stamped.transform.translation.y = atof(argv[2]);
+  geometry_msgs_transform_stamped.transform.translation.z = atof(argv[3]);
   tf2::Quaternion quat;
-  quat.setRPY(atof(argv[5]), atof(argv[6]), atof(argv[7]));
-  static_transformStamped.transform.rotation.x = quat.x();
-  static_transformStamped.transform.rotation.y = quat.y();
-  static_transformStamped.transform.rotation.z = quat.z();
-  static_transformStamped.transform.rotation.w = quat.w();
-
-  static_broadcaster.sendTransform(static_transformStamped);
+  quat.setRPY(atof(argv[4]), atof(argv[5]), atof(argv[6]));
+  geometry_msgs_transform_stamped.transform.rotation.x = quat.x();
+  geometry_msgs_transform_stamped.transform.rotation.y = quat.y();
+  geometry_msgs_transform_stamped.transform.rotation.z = quat.z();
+  geometry_msgs_transform_stamped.transform.rotation.w = quat.w();
+  static_broadcaster.sendTransform(geometry_msgs_transform_stamped);
 
   ros::spin();
 
